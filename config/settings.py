834cc1b434b900f59b643c9bfecb4ef22f61a2eb
divdefwork <divdefwork@gmail.com>
2022-02-09 19:19:33 +0200

    added STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR, ]
+# STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
